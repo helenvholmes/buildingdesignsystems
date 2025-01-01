41cdfc44f6d1a3fcb6d5def89b2dc79c33685d97
Helen V. Holmes <1720093+scooooooooby@users.noreply.github.com>
2024-12-31 21:44:12 -0500

    coming soon

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 
-const { animate, scroll } = Motion
+const { animate, stagger } = Motion
+
+animate("li", { y: [5, 0, 5] },
+  { duration: 2, repeat: Infinity, ease: "easeInOut", delay: stagger(0.1, { ease: [.32, .23, .4, .9] }) })
